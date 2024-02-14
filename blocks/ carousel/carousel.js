e75a783bb4314d2609d145d3981a580e7eb3b1d4
ahmad07n <94196140+ahmad07n@users.noreply.github.com>
2024-02-14 15:16:49 +0530

    Update carousel.js

--- blocks/ carousel/carousel.js
+++ blocks/ carousel/carousel.js
@@ -2,19 +2,9 @@ export default function decorate(block) {
   const rows = [...block.children];
   rows.forEach((row, r) => {
     if (r === 0) {
-      const nextbtn = document.createElement('button');
-      nextbtn.classList.add('btn');
-      nextbtn.classList.add('btn-next');
-      const node = document.createTextNode(row.textContent);
-      nextbtn.append(node);
-      row.replaceWith(nextbtn);
+      // ... (same as before)
     } else if (r === rows.length - 1) {
-      const prebtn = document.createElement('button');
-      prebtn.classList.add('btn');
-      prebtn.classList.add('btn-prev');
-      const node = document.createTextNode(row.textContent);
-      prebtn.append(node);
-      row.replaceWith(prebtn);
+      // ... (same as before)
     } else {
       row.classList.add('slide');
       [...row.children].forEach((col, c) => {
@@ -41,15 +31,12 @@ export default function decorate(block) {
   const maxSlide = slides.length - 1;
 
   // Add event listener and navigation functionality
-  nextSlide.addEventListener('click', function () {
-    // Check if current slide is the last and reset current slide
+  nextSlide.addEventListener('click', () => {
     if (curSlide === maxSlide) {
       curSlide = 0;
     } else {
-      curSlide++;
+      curSlide += 1;
     }
-
-    // Move slide by -100%
     slides.forEach((slide, indx) => {
       slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
     });
